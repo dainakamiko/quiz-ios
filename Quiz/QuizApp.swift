4ed702be2a0cb0a9d65e08f29bcfb4b616e91480
kamiko <d.kamiko@laniakea-tech.com>
2025-04-07 15:30:39 +0900

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by daina on 2025/04/07.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
